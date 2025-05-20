f880881753591552ca645e988eef2144ecf18bf6
NayakYadav1 <yadavnayak202@gmail.com>
2025-05-20 23:53:57 +0545

    Standard library module

---
+++ Day17/Standard_library_module.py
@@ -0,0 +1,19 @@
+# import math module and calculate squareroot
+# import math
+# sqrt = int(math.sqrt(16))
+# print(sqrt)
+
+# print current date and time
+# import datetime
+# now = datetime.datetime.now()
+# print(now)
+
+# list the file in the current directory
+# import os
+# files = os.listdir('../Python')
+# print(files)
+
+# random number from 1 to 100
+import random
+rand = random.randint(1, 100)
+print(rand)
